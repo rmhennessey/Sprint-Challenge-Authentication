0b60e839954c27453cfe57aa860532338021288c
RMH <rory.hennessey@gmail.com>
2019-04-05 11:31:04 -0400

    routes for register and login created

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
 
 const { authenticate } = require('../auth/authenticate');
+
+const Jokes = require('../jokes/jokes-model.js')
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +13,42 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Jokes.add(user)
+    .then(saved => {
+      const token = getJokes(user);
+      res.status(201).json({saved, token})
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+
+  Jokes.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = getJokes(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+
 }
 
 function getJokes(req, res) {
